3467aa96055f3f3c98c13fed72db87531caf036e
kth1999 <koth1999@naver.com>
2023-01-04 21:31:49 +0900

    회원정보예제연습

---
+++ src/회원정보예제/MemberInfo.java
@@ -0,0 +1,11 @@
+package 회원정보예제;
+// 이름, 나이, 직업, 성별의 정보를 입력 받기
+public class MemberInfo {
+    public static void main(String[] args) {
+        Member member=new Member();
+        member.setName();
+        member.setAge();
+        member.setGender();
+    }
+}
+
