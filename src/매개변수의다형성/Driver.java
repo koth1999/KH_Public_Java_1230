45f9978475a1923f90379fe26386f70e320e09b6
kth1999 <koth1999@naver.com>
2023-01-13 20:40:42 +0900

    first commit

---
+++ src/매개변수의다형성/Driver.java
@@ -0,0 +1,15 @@
+package 매개변수의다형성;
+
+public class Driver {
+    String name; // 운전자 이름
+
+    public Driver(String name) {
+        this.name = name;
+    }
+    // 운전
+    public void drive(Vehicle vehicle){
+        System.out.print(name + "의 ");
+        vehicle.run();
+
+    }
+}
