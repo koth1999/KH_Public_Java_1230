d7308797c726b1a45276f7860085b0e186f0928b
kth1999 <koth1999@naver.com>
2023-01-18 21:20:46 +0900

    first commit

--- src/리모컨인터페이스/RemoConInterface.java
+++ src/리모컨인터페이스/RemoConInterface.java
@@ -17,6 +17,40 @@ public class RemoConInterface {
             remoCon.setVolume(120);
             remoCon.getInfo();
         }
+        RemoteControl rc = new RemoteControl() {
+            int volume;
+            @Override
+            public void turnOn() {
+                System.out.println("플레이 스테이션5 켭니다.");
+            }
 
+            @Override
+            public void turnOff() {
+                System.out.println("플레이 스테이션5 끕니다.");
+            }
+
+            @Override
+            public void setVolume(int volume) {
+
+                if(volume>RemoteControl.MAX_VOLUME){
+                    this.volume = RemoteControl.MAX_VOLUME;
+                } else if(volume<RemoteControl.MIN_VOLUME){
+                    this.volume = RemoteControl.MIN_VOLUME;
+                } else{
+                    this.volume = volume;
+                }
+                System.out.println("현재 TV 볼륨 : "+this.volume);
+            }
+
+
+            @Override
+            public void getInfo() {
+                System.out.println("플레이 스테이션5 입니다.");
+                System.out.println("현재 볼륨은 "+volume+" 입니다.");
+            }
+        };
+        rc.turnOn();
+        rc.setVolume(100);
+        rc.getInfo();
     }
 }
