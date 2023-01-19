7fe1b1c58626866e3b1f714c61b8db0250ac871a
kth1999 <koth1999@naver.com>
2023-01-19 20:33:48 +0900

    first commit

---
+++ src/자동차종합예제/Bus.java
@@ -0,0 +1,18 @@
+package 자동차종합예제;
+
+public class Bus extends Car{
+    public Bus(String name) {
+        this.name = name;
+        maxSpeed = 150;
+        fuelEff = 5; // 연비
+        fuelTank = 100;
+        seatCnt = 20;
+    }
+
+    @Override
+    void setMode(boolean isMode) {
+        if(isMode) fuelTank+=30;
+    }
+}
+
+
