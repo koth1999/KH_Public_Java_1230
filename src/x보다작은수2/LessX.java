1fedce3266fed19b9755e16807b3ac1ffc7d3195
kth1999 <koth1999@naver.com>
2023-02-02 21:29:14 +0900

    230202

--- src/x보다작은수2/LessX.java
+++ src/x보다작은수2/LessX.java
@@ -1,21 +1,18 @@
 package x보다작은수2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LessX {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); // 배열의 갯수
-        int X = sc.nextInt(); // 주어진 수
-
-        int[] arr = new int[N];
-
-        for(int i=0; i<arr.length; i++){
-            arr[i] = sc.nextInt(); // 주어진 N만큼 받기
-            if(arr[i]<X){
-                System.out.print(arr[i]+" ");
-            }
-            System.out.println();
+        List<Integer> list = new ArrayList<>();
+        int N = sc.nextInt();
+        int X = sc.nextInt();
+        for(int i=0; i<N; i++){
+            list.add(sc.nextInt());
+            if(list.get(i)<X) System.out.print(list.get(i)+" ");
         }
     }
 }
