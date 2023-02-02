68ff80e0aff2a197eb24bf9cb27239d19494bf7f
kth1999 <koth1999@naver.com>
2023-02-02 17:47:29 +0900

    230202

--- src/피타고라스정리/Pythagoras.java
+++ src/피타고라스정리/Pythagoras.java
@@ -1,26 +1,27 @@
 package 피타고라스정리;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
-
-
 
 public class Pythagoras {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int[] arr = new int[3];
-        while(true){
-            for(int i=0; i<arr.length; i++){
-                arr[i]=sc.nextInt();
+        List<String> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int sum = 0;
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+                sum+=arr[i];
             }
+            if(sum==0) break;
             Arrays.sort(arr);
-
-            if(arr[0]+arr[1]+arr[2]==0) break;
-            else if((arr[2]*arr[2]) == (arr[0]*arr[0])+(arr[1]*arr[1])){
-                System.out.println("right");
-            } else{
-                System.out.println("wrong");
-            }
+            if((arr[2]*arr[2])==(arr[0]*arr[0])+(arr[1]*arr[1])){
+                list.add("right");
+            } else list.add("wrong");
         }
+        for(String e : list) System.out.println(e);
     }
 }
